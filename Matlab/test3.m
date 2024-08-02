eadb94046383d9ad2d35e695819c5e1af84ab1aa
Cesinhacas <cesar.alcantara.souza@uel.br>
2024-08-02 18:22:37 -0300

    Sem muitos avanços no TWOSTEP

--- Matlab/test3.m
+++ Matlab/test3.m
@@ -5,36 +5,40 @@ mz = Dados_Corrompido(3,:);
 
 for i=1:1:length(mx)
     i_vec = [mx(i) my(i) mz(i)]';
-    MU(i) = sum(var([mx(i) 0 0; 0 my(i) 0; 0 0 mz(i)]));
-    sigma_m(i,:) = var([mx(i) 0 0; 0 my(i) 0; 0 0 mz(i)]);
-    sigma(i) =  1/(4*i_vec'*diag(sigma_m(i,:))*i_vec + 2*trace(diag(sigma_m(i,:).^2)));
+    cov_M = [0.006^2 0 0;0 0.006^2 0;0 0 0.006^2];
+    MU(i) = -trace(cov_M);
+    sigma(i) = (4*0.006^2*(i_vec')*i_vec + 6*(0.006^4));
     Z(i) = norm(i_vec)^2 - 1;
     L(i,:) = [2*i_vec' -mx(i)^2 -my(i)^2 -mz(i)^2 -2*mx(i)*my(i) -2*mx(i)*mz(i) -2*my(i)*mz(i)];
 end
 
+SIGMA = 0;
+for i=1:1:length(mx)
+    SIGMA = SIGMA + 1/sigma(i);
+end
+SIGMA = 1/SIGMA;
+
 Z_ =0;
 L_ = [0 0 0 0 0 0 0 0 0];
 MU_ =0;
-SIGMA = 1/sum(sigma);
 
 for i=1:1:length(mx)
-    Z_ = Z_ + SIGMA*sum(Z(i)/sigma(i));
-    L_ = L_ + SIGMA*sum(L(i,:)/sigma(i));
+    Z_ = Z_ + SIGMA*Z(i)/sigma(i);
+    L_ = L_ + SIGMA*L(i,:)/sigma(i);
     MU_ = MU_ + SIGMA*(MU(i)/sigma(i));
 end
 
-
-
 for i=1:1:length(mx)
-    MU(i) = MU(i)-MU_;
+    MU(i) = MU(i) - MU_;
     Z(i) = Z(i) - Z_;
     L(i,:) = L(i,:) - L_;
 end
+
 covariance = zeros(9);
 estim = zeros(9,1);
 for i=1:1:length(mx)
-    estim = estim + sigma(i)*(Z(i) - MU(i))*L(i,:)';
-    covariance = covariance + L(i,:)'*L(i,:).*sigma(i);
+    estim = estim + (1/sigma(i))*(Z(i) - MU(i))*L(i,:)';
+    covariance = covariance + L(i,:)'*L(i,:)./sigma(i);
 end
 
 estim = covariance\estim;
@@ -57,6 +61,12 @@ passo = 0;
      theta = theta - (inv(covariance)+ 1/SIGMA*(L_ - phi)'*(L_ - phi))\g;
      n_i = (theta - estim)'*(covariance + (1/SIGMA) * (L_ - phi)'*(L_-phi)) * (theta - estim);
      estim = theta;
+     
+     c = [estim(1); estim(2); estim(3)];
+     E = [estim(4) estim(7) estim(8);
+        estim(7) estim(5) estim(9);
+        estim(8) estim(9) estim(6)];
+
      if n_i < n
          loop = 0;
      end
@@ -73,5 +83,9 @@ W = diag([-1+sqrt(1+S(1,1)) -1+sqrt(1+S(2,2)) -1+sqrt(1+S(3,3))]);
 D = U*W*V';
 h = (eye(3)+D)\c;
 
+teste = 0;
+for i=1:1:length(mx)
+    teste = teste + Z(i)*sigma(i);
+end
 
 end
