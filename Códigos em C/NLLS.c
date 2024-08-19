18d9a9569b3c13dbb4283a5d98bc4a781b213227
Cesinhacas <cesar.alcantara.souza@uel.br>
2024-08-19 18:43:29 -0300

    Coisas estranhas no NLLS

--- Códigos em C/NLLS.c
+++ Códigos em C/NLLS.c
@@ -100,26 +100,13 @@ void main()
             error_ant = J;
         }
 
-        for(int i=0; i < tam; i++)
+        for(int i =0; i < tam; i++)
         {
             H[0][i] = (2*(sx*(by - my[i]) - sy*sin(rho)*(bx - mx[i]))*(by - my[i]))/(sx*sx*sy*sy*cos(rho)*cos(rho)) - (2*pow((sx*(by - my[i]) - sy*sin(rho)*(bx - mx[i])), 2))/(pow(sx, 3)*sy*sy*cos(rho)*cos(rho)) - (2*pow((bx - mx[i]), 2))/pow(sx, 3) - (2*pow((sy*sz*(sin(lambida)*sin(rho) - cos(lambida)*cos(rho)*sin(phi))*(bx - mx[i]) + sx*sy*cos(rho)*(bz - mz[i]) - sx*sz*sin(lambida)*(by - my[i])), 2))/(pow(sx, 3)*sy*sy*sz*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*cos(rho)*cos(rho)) + (2*(sy*cos(rho)*(bz - mz[i]) - sz*sin(lambida)*(by - my[i]))*(sy*sz*(sin(lambida)*sin(rho) - cos(lambida)*cos(rho)*sin(phi))*(bx - mx[i]) + sx*sy*cos(rho)*(bz - mz[i]) - sx*sz*sin(lambida)*(by - my[i])))/(sx*sx*sy*sy*sz*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*cos(rho)*cos(rho));
 
             H[1][i] = -(2*(by - my[i])*(by*sx*sz*sin(lambida)*sin(lambida) - my[i]*sx*sz*sin(lambida)*sin(lambida) + by*sx*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi) - my[i]*sx*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi) - bz*sx*sy*cos(rho)*sin(lambida) + mz[i]*sx*sy*cos(rho)*sin(lambida) - bx*sy*sz*sin(lambida)*sin(lambida)*sin(rho) + mx[i]*sy*sz*sin(lambida)*sin(lambida)*sin(rho) - bx*sy*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*sin(rho) + mx[i]*sy*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*sin(rho) + bx*sy*sz*cos(lambida)*cos(rho)*sin(lambida)*sin(phi) - mx[i]*sy*sz*cos(lambida)*cos(rho)*sin(lambida)*sin(phi)))/(sx*pow(sy, 3)*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*cos(rho)*cos(rho));
-
-            H[2][i] = -(2*(bz - mz[i])*(bz*sx*sy*cos(rho) - by*sx*sz*sin(lambida) - mz[i]*sx*sy*cos(rho) + my[i]*sx*sz*sin(lambida) + bx*sy*sz*sin(lambida)*sin(rho) - mx[i]*sy*sz*sin(lambida)*sin(rho) - bx*sy*sz*cos(lambida)*cos(rho)*sin(phi) + mx[i]*sy*sz*cos(lambida)*cos(rho)*sin(phi)))/(sx*sy*pow(sz, 3)*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*cos(rho));
-
-            H[3][i] = (2*bx - 2*mx[i])/sx*sx - (2*sin(rho)*(sx*(by - my[i]) - sy*sin(rho)*(bx - mx[i])))/(sx*sx*sy*cos(rho)*cos(rho)) + (2*(sin(lambida)*sin(rho) - cos(lambida)*cos(rho)*sin(phi))*(sy*sz*(sin(lambida)*sin(rho) - cos(lambida)*cos(rho)*sin(phi))*(bx - mx[i]) + sx*sy*cos(rho)*(bz - mz[i]) - sx*sz*sin(lambida)*(by - my[i])))/(sx*sx*sy*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*cos(rho)*cos(rho));
-
-            H[4][i] = (2*(sx*(by - my[i]) - sy*sin(rho)*(bx - mx[i])))/(sx*sy*sy*cos(rho)*cos(rho)) - (2*sin(lambida)*(sy*sz*(sin(lambida)*sin(rho) - cos(lambida)*cos(rho)*sin(phi))*(bx - mx[i]) + sx*sy*cos(rho)*(bz - mz[i]) - sx*sz*sin(lambida)*(by - my[i])))/(sx*sy*sy*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*cos(rho)*cos(rho));
-
-            H[5][i] = (2*(sy*sz*(sin(lambida)*sin(rho) - cos(lambida)*cos(rho)*sin(phi))*(bx - mx[i]) + sx*sy*cos(rho)*(bz - mz[i]) - sx*sz*sin(lambida)*(by - my[i])))/(sx*sy*sz*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*cos(rho));
-
-            H[6][i] = -(2*(bx*sy - mx[i]*sy - by*sx*sin(rho) + my[i]*sx*sin(rho))*(by*sx*sz*sin(lambida)*sin(lambida) - my[i]*sx*sz*sin(lambida)*sin(lambida) + by*sx*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi) - my[i]*sx*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi) - bz*sx*sy*cos(rho)*sin(lambida) + mz[i]*sx*sy*cos(rho)*sin(lambida) - bx*sy*sz*sin(lambida)*sin(lambida)*sin(rho) + mx[i]*sy*sz*sin(lambida)*sin(lambida)*sin(rho) - bx*sy*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*sin(rho) + mx[i]*sy*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*sin(rho) + bx*sy*sz*cos(lambida)*cos(rho)*sin(lambida)*sin(phi) - mx[i]*sy*sz*cos(lambida)*cos(rho)*sin(lambida)*sin(phi)))/(sx*sx*sy*sy*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*pow(cos(rho), 3));
-
-            H[7][i] = (2*sin(phi)*pow((sy*sz*(sin(lambida)*sin(rho) - cos(lambida)*cos(rho)*sin(phi))*(bx - mx[i]) + sx*sy*cos(rho)*(bz - mz[i]) - sx*sz*sin(lambida)*(by - my[i])), 2))/(sx*sx*sy*sy*sz*sz*cos(lambida)*cos(lambida)*pow(cos(phi), 3)*cos(rho)*cos(rho)) - (2*(bx - mx[i])*(sy*sz*(sin(lambida)*sin(rho) - cos(lambida)*cos(rho)*sin(phi))*(bx - mx[i]) + sx*sy*cos(rho)*(bz - mz[i]) - sx*sz*sin(lambida)*(by - my[i])))/(sx*sx*sy*sz*cos(lambida)*cos(phi)*cos(rho));
-
-            H[8][i] = (2*sin(lambida)*pow((sy*sz*(sin(lambida)*sin(rho) - cos(lambida)*cos(rho)*sin(phi))*(bx - mx[i]) + sx*sy*cos(rho)*(bz - mz[i]) - sx*sz*sin(lambida)*(by - my[i])), 2))/(sx*sx*sy*sy*sz*sz*pow(cos(lambida), 3)*cos(phi)*cos(phi)*cos(rho)*cos(rho)) + (2*(sy*sz*(cos(lambida)*sin(rho) + cos(rho)*sin(lambida)*sin(phi))*(bx - mx[i]) - sx*sz*cos(lambida)*(by - my[i]))*(sy*sz*(sin(lambida)*sin(rho) - cos(lambida)*cos(rho)*sin(phi))*(bx - mx[i]) + sx*sy*cos(rho)*(bz - mz[i]) - sx*sz*sin(lambida)*(by - my[i])))/(sx*sx*sy*sy*sz*sz*cos(lambida)*cos(lambida)*cos(phi)*cos(phi)*cos(rho)*cos(rho));
         }
+        
         float Ht_h[N][N];
         Ht_h[1][1] = 0;
         passo++;
