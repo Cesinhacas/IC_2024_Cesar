fe56650c9299d2cad404835036e20a44e96cf4c5
Cesinhacas <cesar.alcantara.souza@uel.br>
2024-10-25 16:10:40 -0300

    Pasta build para a main

---
+++ ccodes/build/CMakeFiles/NLLS.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NLLS.dir/NLLS.c.o"
+  "CMakeFiles/NLLS.dir/NLLS.c.o.d"
+  "NLLS"
+  "NLLS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NLLS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ccodes/build/CMakeFiles/NLLS.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for NLLS.
+# This may be replaced when dependencies are built.
