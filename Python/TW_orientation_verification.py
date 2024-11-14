dd4c1b6c3fa1d6d66925750686911a5585093dd4
Cesinhacas <cesar.alcantara.souza@uel.br>
2024-11-14 16:17:25 -0300

    Criando arquivos .csv para identificar se os dados do TS estão rotacionados

--- Python/TW_orientation_verification.py
+++ Python/TW_orientation_verification.py
@@ -1,17 +1,24 @@
 import numpy as np
 import pandas as pd
 
-dados_NLLS = pd.read_csv('c:/Users/labt5/OneDrive/Desktop/Cesar/IC_2024_Cesar/Dados/Dados_rest_NLLS.csv', header=None) #/mnt/c/users/labt5/desktop/cesar/IC_2024_Cesar/dados/Dados_Corrompido.csv
+dados = pd.read_csv('c:/Users/labt5/OneDrive/Desktop/Cesar/IC_2024_Cesar/Dados/Dados_Teoricos.csv', header=None) #/mnt/c/users/labt5/desktop/cesar/IC_2024_Cesar/dados/Dados_Corrompido.csv
 dados_TW = pd.read_csv('c:/Users/labt5/OneDrive/Desktop/Cesar/IC_2024_Cesar/Dados/Dados_rest_TW.csv', header=None) #/mnt/c/users/labt5/desktop/cesar/IC_2024_Cesar/dados/Dados_Corrompido.csv
+dados_NLLS = pd.read_csv('c:/Users/labt5/OneDrive/Desktop/Cesar/IC_2024_Cesar/Dados/Dados_rest_NLLS.csv', header=None) #/mnt/c/users/labt5/desktop/cesar/IC_2024_Cesar/dados/Dados_Corrompido.csv
 
-rest_NLLS = dados_NLLS.values
+dados = dados.values
 rest_TW = dados_TW.values
+rest_NLLS = dados_NLLS.values
 
-tam = len(rest_NLLS[0])
-dif_norm = np.zeros(tam)
+tam = len(dados[0])
+dif_norm_TW = np.zeros((tam,3))
+dif_norm_NLLS = np.zeros((tam,3))
 
 for i in range(tam):
-    dif_norm[i] = np.linalg.norm(rest_NLLS[:, i] - rest_TW[:, i])
+    dif_norm_TW[i] = dados[:, i] - rest_TW[:, i]
+    dif_norm_NLLS[i] = dados[:, i] - rest_NLLS[:, i]
 
-dif_norm = pd.DataFrame(dif_norm)
-dif_norm.to_csv('c:/Users/labt5/OneDrive/Desktop/Cesar/IC_2024_Cesar/Dados/Dif_norm.csv', header=False)
+dif_norm_TW = pd.DataFrame(dif_norm_TW)
+dif_norm_TW.to_csv('c:/Users/labt5/OneDrive/Desktop/Cesar/IC_2024_Cesar/Dados/Dif_norm.csv', header=False)
+
+dif_norm_NLLS = pd.DataFrame(dif_norm_NLLS)
+dif_norm_NLLS.to_csv('c:/Users/labt5/OneDrive/Desktop/Cesar/IC_2024_Cesar/Dados/Dif_norm_NLLS.csv', header=False)
