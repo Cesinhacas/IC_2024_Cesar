d7dd9942c2d0929eb699fad5a101b743fdcdae79
Cesinhacas <cesar.alcantara.souza@uel.br>
2025-08-13 17:07:55 -0300

    Update calib_data_mag_accel.m

--- Matlab/calib_data_mag_accel.m
+++ Matlab/calib_data_mag_accel.m
@@ -8,9 +8,9 @@ my = data(:,3);
 mz = data(:,4);
 
 Dados_Corrompido = [mx,my,mz]';
-[Time,p0] = test1(Dados_Corrompido, 0.23);
+[Time,p] = test1(Dados_Corrompido, 0.23);
 disp(Time)
-disp(p0)
+disp(p)
 
 hFig = figure;
 set(hFig, 'Position', [100 100 700*3/2 300])
@@ -20,8 +20,8 @@ screenposition = get(gcf,'Position');
 set(gcf,...
 'PaperPosition',[0 0 screenposition(3:4)],...
 'PaperSize',[screenposition(3:4)]);
- e = r;
- a = 1*e;
+ e = 0.23;
+ a = e;
 [x,y,z] = sphere;
 x = e*x;
 y = e*y;
@@ -35,14 +35,15 @@ axis equal
 set(gca,'PlotBoxAspectRatioMode','manual')
 set(gca,'FontSize',12)
 hold on
-mx = Dados_Teoricos(1,:);
-my = Dados_Teoricos(2,:);
-mz = Dados_Teoricos(3,:);
 plot3(mx,my,mz,'r.','linewidth',1.5)
 title('(a)'); 
 xlabel('x axis (G)');
 ylabel('y axis (G)');
 zlabel('z axis (G)');
+
+Dados_rest(1,:) =  (Dados_Corrompido(1,:) - p(4))/p(1);
+Dados_rest(2,:) = ((Dados_Corrompido(2,:) - p(5))/p(2) - Dados_rest(1,:)*sin(p(7)))/cos(p(7));
+Dados_rest(3,:) = ((Dados_Corrompido(3,:) - p(6))/p(3) - Dados_rest(1,:)*sin(p(8))*cos(p(9)) - Dados_rest(2,:)*sin(p(9)))/(cos(p(8))*cos(p(9)));
 
 subplot(2,2,2)
 set(gcf,'Units','inches');
@@ -50,8 +51,8 @@ screenposition = get(gcf,'Position');
 set(gcf,...
 'PaperPosition',[0 0 screenposition(3:4)],...
 'PaperSize',[screenposition(3:4)]);
- e = r;
- a = 1.1*e;
+ e = 0.23;
+ a = 0.23;
 [x,y,z] = sphere;
 x = e*x;
 y = e*y;
@@ -65,9 +66,9 @@ axis equal
 set(gca,'PlotBoxAspectRatioMode','manual')
 set(gca,'FontSize',12)
 hold on
-mx = Dados_Corrompido(1,:);
-my = Dados_Corrompido(2,:);
-mz = Dados_Corrompido(3,:);
+mx = Dados_rest(1,:);
+my = Dados_rest(2,:);
+mz = Dados_rest(3,:);
 plot3(mx,my,mz,'r.','linewidth',1.5)
 title('(b)'); 
 xlabel('x axis (G)');
@@ -76,15 +77,9 @@ zlabel('z axis (G)');
 
 data = readmatrix('acelerometro_data_20250813_155734.csv');
 
-for i=1:1:length(data)
-    vet = [data(i, 2), data(i,3), data(i,4)];
-    if norm(vet) > 1.2
-        mx = data(:,2);
-        my = data(:,3);
-        mz = data(:,4);
-    end
-
-end
+mx = data(:,2);
+my = data(:,3);
+mz = data(:,4);
 
 Dados_Corrompido = [mx,my,mz]';
 [Time,p1] = test1(Dados_Corrompido, 1);
@@ -99,37 +94,7 @@ screenposition = get(gcf,'Position');
 set(gcf,...
 'PaperPosition',[0 0 screenposition(3:4)],...
 'PaperSize',[screenposition(3:4)]);
- e = r;
- a = 1.1*e;
-[x,y,z] = sphere;
-x = e*x;
-y = e*y;
-z = e*z;
-s2 = surf(x,y,z);
-set(s2,'FaceColor','none')
-set(s2,'EdgeColor',[0.7 0.7 0.7])
-hold on
-axis([-a a -a a -a a])
-axis equal
-set(gca,'PlotBoxAspectRatioMode','manual')
-set(gca,'FontSize',12)
-hold on
-mx = Dados_Teoricos(1,:);
-my = Dados_Teoricos(2,:);
-mz = Dados_Teoricos(3,:);
-plot3(mx,my,mz,'r.','linewidth',1.5)
-title('(a)'); 
-xlabel('x axis (G)');
-ylabel('y axis (G)');
-zlabel('z axis (G)');
-
-subplot(2,2,2)
-set(gcf,'Units','inches');
-screenposition = get(gcf,'Position');
-set(gcf,...
-'PaperPosition',[0 0 screenposition(3:4)],...
-'PaperSize',[screenposition(3:4)]);
- e = r;
+ e = 1;
  a = 1.1*e;
 [x,y,z] = sphere;
 x = e*x;
@@ -147,6 +112,40 @@ hold on
 mx = Dados_Corrompido(1,:);
 my = Dados_Corrompido(2,:);
 mz = Dados_Corrompido(3,:);
+plot3(mx,my,mz,'r.','linewidth',1.5)
+title('(a)'); 
+xlabel('x axis (G)');
+ylabel('y axis (G)');
+zlabel('z axis (G)');
+
+Dados_rest(1,:) =  (Dados_Corrompido(1,:) - p1(4))/p1(1);
+Dados_rest(2,:) = ((Dados_Corrompido(2,:) - p1(5))/p1(2) - Dados_rest(1,:)*sin(p1(7)))/cos(p1(7));
+Dados_rest(3,:) = ((Dados_Corrompido(3,:) - p1(6))/p1(3) - Dados_rest(1,:)*sin(p1(8))*cos(p1(9)) - Dados_rest(2,:)*sin(p1(9)))/(cos(p1(8))*cos(p1(9)));
+
+subplot(2,2,2)
+set(gcf,'Units','inches');
+screenposition = get(gcf,'Position');
+set(gcf,...
+'PaperPosition',[0 0 screenposition(3:4)],...
+'PaperSize',[screenposition(3:4)]);
+ e = 1;
+ a = 1.1*e;
+[x,y,z] = sphere;
+x = e*x;
+y = e*y;
+z = e*z;
+s2 = surf(x,y,z);
+set(s2,'FaceColor','none')
+set(s2,'EdgeColor',[0.7 0.7 0.7])
+hold on
+axis([-a a -a a -a a])
+axis equal
+set(gca,'PlotBoxAspectRatioMode','manual')
+set(gca,'FontSize',12)
+hold on
+mx = Dados_rest(1,:);
+my = Dados_rest(2,:);
+mz = Dados_rest(3,:);
 plot3(mx,my,mz,'r.','linewidth',1.5)
 title('(b)'); 
 xlabel('x axis (G)');
